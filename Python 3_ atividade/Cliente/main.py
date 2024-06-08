8f11e46a5267c76ba96da114fc9fbb4c30ae2c30
Sophia Werneck <78826752+sophiawerneck@users.noreply.github.com>
2024-06-08 20:11:29 -0300

    Update main.py

--- Python 3_ atividade/Cliente/main.py
+++ Python 3_ atividade/Cliente/main.py
@@ -1,5 +1,6 @@
 from cliente import Cliente
 
 c = Cliente("127.0.0.1",9000)
-caminho_imagem = 'C:\Users\werne\OneDrive\Documentos\informatica_ind\InformaticaIndustrialUFJF-main\Python\Python 3\ExemploProcessamentoImagem\faces\image_0001.jpg'
+caminho_imagem = 'faces/image_0003.jpg'
 c.start(caminho_imagem) 
+
