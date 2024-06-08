d27341d03b759ca9220197186ccd56aabd871353
Sophia Werneck <78826752+sophiawerneck@users.noreply.github.com>
2024-06-08 20:14:59 -0300

    Update servidor.py

--- Python 3_ atividade/Servidor/servidor.py
+++ Python 3_ atividade/Servidor/servidor.py
@@ -15,7 +15,7 @@ class Servidor():
         self._port = port
         self.__tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #tcp é o objeto que instanciou na classe socket
         #primeiro parametro é a família de endereços para qual quer oferecer o serviço, segundo parametro indica se o serviço é TCP ou UDP, no caso
-        #é TCP, se for UDP é SOCK_DGRAM
+        #é TCP, se for UDP é SOCK_DGRAMself.__tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     def start(self): #da início ao processo do servidor
         """
         Método que inicializa a execução do servidor
@@ -41,30 +41,30 @@ class Servidor():
         print("Atendendo cliente ", client)
         while True: #deixa o cliente realizar quantas operações ele quiser
             try:  #no cliente manda primeiro e depois recebe, no servidor recebe primeiro depois manda
+                # Recebe o tamanho da imagem
                 tam_img = con.recv(1024)
                 tam = int.from_bytes(tam_img, 'big')
+                # Recebe a imagem
                 cont = 1024
                 img_rec = con.recv(1024)
-
                 while(cont<=tam):
                     img_rec += con.recv(1024)
                     cont += 1024
                 img = cv2.imdecode(np.frombuffer(img_rec, np.uint8), cv2.IMREAD_COLOR)
-                # processamento
-                xml_classificador = os.path.join(os.path.relpath(
-                   cv2.__file__).replace('__init__.py', ''), 'data\haarcascade_frontalface_default.xml')
-                face_cascade = cv2.CascadeClassifier(
-                   xml_classificador)
+                # Processamento da imagem
+                xml_classificador = os.path.join(os.path.relpath(cv2.__file__).replace('__init__.py', ''), 'data\haarcascade_frontalface_default.xml')
+                face_cascade = cv2.CascadeClassifier(xml_classificador)
                 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
                 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
                 
                 # Desenha retângulos nas áreas onde as faces foram detectadas
                 for (x, y, w, h) in faces:
-                   cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-                # codificação para bytes
+                   cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 255), 2) # BGR = (blue, green, red) (255, 0, 255) é rosa
+                # Codificação para bytes 
                 _, img_bytes = cv2.imencode('.jpg', img)
                 img_bytes = bytes(img_bytes)
                 tamanho_da_imagem_codificado = len(img_bytes).to_bytes(4, 'big')
+                # Envio da imagem processada
                 con.send(tamanho_da_imagem_codificado)
                 con.send(img_bytes)
                 print(client, " -> requisição atendida")
@@ -73,8 +73,7 @@ class Servidor():
                 print("Erro de conexão ", client, ": ", e.args)
                 return
             except Exception as e:
-                print("Erro nos dados recebidos pelo cliente ",
-                      client, ": ", e.args)
+                print("Erro nos dados recebidos pelo cliente ", client, ": ", e.args)
                 con.send(bytes("Erro", 'ascii'))
                 return
 
