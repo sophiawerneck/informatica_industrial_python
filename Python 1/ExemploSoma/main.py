d5878a46a20e364700bd4e783a080b9bbd35fb61
Sophia Werneck <78826752+sophiawerneck@users.noreply.github.com>
2024-06-09 18:13:18 -0300

    Update main.py

--- Python 1/ExemploSoma/main.py
+++ Python 1/ExemploSoma/main.py
@@ -5,4 +5,8 @@ print("Digite o segundo operando:",end='')
 b = int(input())
 print(f'Resultado da soma: {a+b}')#O f é utilizado para possibilitar a utilização dos { }
 
+# Ou:
+x = int(input("1º número: "))
+y = int(input("2º número: "))
+print(f"Soma: {x+y}")
 
