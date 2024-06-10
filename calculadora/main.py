bbb66b7be08df8ffd4526b7b670fe74bd1809754
Sophia Werneck <78826752+sophiawerneck@users.noreply.github.com>
2024-06-10 17:08:40 -0300

    Update main.py

--- calculadora/main.py
+++ calculadora/main.py
@@ -5,6 +5,46 @@ from kivy.config import Config
 from kivy.uix.gridlayout import GridLayout
 from kivy.core.window import Window
 
+class MyWidget(BoxLayout):
+    expression = ""  # String para armazenar a expressão
+
+    def armazena(self, num):
+        """
+        Método para funções da calculadora
+        """
+        if num == 'C':
+            self.expression = ""
+            self.ids.lb.text = '0'
+        elif num == 'DEL':
+            self.expression = self.expression[:-1]
+            self.ids.lb.text = self.expression if self.expression else '0'
+        elif num == '=':
+            try:
+                self.expression = str(eval(self.expression))
+                self.ids.lb.text = self.expression
+            except Exception as e:
+                self.ids.lb.text = 'Erro'
+                self.expression = ""
+        else:
+            if self.ids.lb.text == '0':
+                self.expression = num
+            else:
+                self.expression += num
+            self.ids.lb.text = self.expression
+
+class CalculadoraApp(App):
+    def build(self):
+        """
+        Método para construção do aplicativo com base no widget criado
+        """
+        return MyWidget()  # Retorna um objeto da classe MyWidget
+
+if __name__ == '__main__':
+    Config.set('graphics', 'resizable', True)
+    Window.size = (500, 600)
+    Window.fullscreen = False
+    CalculadoraApp().run()
+'''
 # Usar o eval para fazer as contas
 class MyWidget(BoxLayout):
     def armazena(self,dt):
@@ -72,11 +112,12 @@ if __name__ == '__main__':
     Window.fullscreen = False
     CalculadoraApp().run()
 
-# Para calculadora: layoult box layolt com 3, a tela(0 q é label), outro para os outros numeros e o último para a linha de baixo
-# grid layout, só utiliza label e botão
-'''
+ Para calculadora: layoult box layolt com 3, a tela(0 q é label), outro para os outros numeros e o último para a linha de baixo
+ grid layout, só utiliza label e botão
+
 Atividade: primeiro um label em cima pra mostrar o resultados
 Depois um grid layout para os quadrados iguais do meio 
 Depois um box layout para os dois com tamanho diferente embaixo 
-Organizar layouts, label e botões no .kv'''
-#self.ids['lb'].text = str(int(self.ids.lb.text) + 1)  # self.ids.lb.text tem acesso aos ids dos componentes do arquivo .kv
+Organizar layouts, label e botões no .kv
+self.ids['lb'].text = str(int(self.ids.lb.text) + 1)  # self.ids.lb.text tem acesso aos ids dos componentes do arquivo .kv
+'''
