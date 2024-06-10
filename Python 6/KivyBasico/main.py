0d1b2e82104d59b9882368583f4d8b14db0c20b3
Sophia Werneck <78826752+sophiawerneck@users.noreply.github.com>
2024-06-10 15:24:38 -0300

    Update main.py

--- Python 6/KivyBasico/main.py
+++ Python 6/KivyBasico/main.py
@@ -1,7 +1,7 @@
 import kivy
-from kivy.app import App
+from kivy.app import App # Classe base a geração de aplicativos utilizando o kivy
 from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.boxlayout import BoxLayout # BoxLayoult permite organizar os widgets de maneira sequencial
 from kivy.uix.label import Label
 
 class BasicApp(App):
@@ -12,20 +12,20 @@ class BasicApp(App):
         """
         Constrói o aplicativo a partir de um conjunto de widgets
         :return Widget principal da aplicação
-        """
-        layout = BoxLayout(orientation = 'horizontal')
-        self.lb = Label(text="0")
-        bt = Button(text = "Botão 1", on_release = self.incrementar)
-        layout.add_widget(self.lb)
-        layout.add_widget(bt)
+        """ # Dentro do layoult tem 3 widgets: label, botao e layoult 2
+        layout = BoxLayout(orientation = 'horizontal') # widget base: layoult
+        self.lb = Label(text="0") # label = widget filho 1
+        bt = Button(text = "Botão 1", on_release = self.incrementar) # label = widget filho 2
+        layout.add_widget(self.lb) # Insere o label
+        layout.add_widget(bt) # Insere o botão
         
         
-        layout2 = BoxLayout(orientation = 'vertical')
+        layout2 = BoxLayout(orientation = 'vertical') # layoult 2 tem 2 widgets
         self.lb2 = Label(text="0", color=[1,0,0,1])
         self.lb3 = Label(text="0")
         layout2.add_widget(self.lb2)
         layout2.add_widget(self.lb3)
-        layout.add_widget(layout2)
+        layout.add_widget(layout2) # Adicionar o layoult 2 como filho do layoult
 
         return layout
     
@@ -36,3 +36,4 @@ class BasicApp(App):
 
 if __name__=='__main__':
     BasicApp().run()
+
